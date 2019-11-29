6835cf436677020e4668fe1ac634cc0b4b2fb546
Andrey Alekseenko <al42and@gmail.com>
2019-11-28 23:57:05 -0500

    server: Added missing values to the example config
    Without them, old points were not reaped

--- server/config.example.js
+++ server/config.example.js
@@ -1,3 +1,5 @@
 exports.browserPort = 9100;
 exports.restPort = 9201;
 exports.mongo = 'mongodb://mongo:27017/gps-web-tracker';
+exports.pointTTL = 300000; // 3e5 = 5 minutes
+exports.pointCheckTime = 60000; // 6e4 = 1 minute
